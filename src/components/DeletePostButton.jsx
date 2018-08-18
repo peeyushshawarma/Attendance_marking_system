466dd9508b0c044db95cd62824b6d42415fa267f
peeyush Sharma <peeyushs37@gmail.com>
2018-08-18 15:04:49 +0530

    First commit

---
+++ src/components/DeletePostButton.jsx
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {postRef} from '../firebase';
+
+class DeletePostButton extends Component{
+
+	deleteItem(e){
+
+	 	 const{email,posttitle} =this.props.post;
+		 const {serverKey}= this.props;
+		 postRef.child(serverKey).remove()
+		 console.log('post removed');	//to remove the particular post from post list
+	}
+	render(){
+
+		return(
+			
+			    <button className='btn btn-xs btn-danger' 
+			            style={{marginLeft:'10px'}}
+			            onClick={(e) => { if (window.confirm('Are you sure, you want to delete this post?')) this.deleteItem(e) } }
+			    >
+			        Delete
+			    </button>
+
+			
+		);	
+	}
+}
+
+export default DeletePostButton;
